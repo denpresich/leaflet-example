6c68495d8fa708e18f2259a37f073bea8d03e59d
Denys Presich <den.presich@icloud.com>
2022-09-10 11:51:48 +0300

    Add images to places

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const places = [
   {
     name: "Човникова станція",
+    imageUrl:
+      "https://ua.igotoworld.com/frontend/webcontent/images/tours/1940195_800x600_ParkPravdi.jpg",
     coordinates: {
       lat: 47.897872826697785,
       lng: 33.33298162115136,
@@ -8,6 +10,8 @@ const places = [
   },
   {
     name: "Арт майдан",
+    imageUrl:
+      "https://ua.igotoworld.com/frontend/webcontent/images/tours/1940198_800x600_Art-Maidan_u_gorodskogo_teatra.jpg",
     coordinates: {
       lat: 47.904592186707376,
       lng: 33.34175971008661,
@@ -15,6 +19,8 @@ const places = [
   },
   {
     name: "Квітковий годинник",
+    imageUrl:
+      "https://ua.igotoworld.com/frontend/webcontent/images/tours/1940201_800x600_zvet1-b.jpg",
     coordinates: {
       lat: 47.905200785590274,
       lng: 33.39208067967246,
@@ -22,6 +28,8 @@ const places = [
   },
   {
     name: "Музей гірничої техніки під відкритим небом",
+    imageUrl:
+      "https://ua.igotoworld.com/frontend/webcontent/images/tours/1940696_800x600_mine_001.jpg",
     coordinates: {
       lat: 48.15004075741973,
       lng: 33.584965100845366,
@@ -29,6 +37,8 @@ const places = [
   },
   {
     name: "Академія руху",
+    imageUrl:
+      "https://ua.igotoworld.com/frontend/webcontent/images/tours/1940210_800x600_teatr.jpg",
     coordinates: {
       lat: 48.00003585422228,
       lng: 33.44884598570862,
@@ -48,14 +58,19 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap",
 }).addTo(map);
 
-places.map(({ name, coordinates: { lat, lng } }) => {
+places.map(({ name, imageUrl, coordinates: { lat, lng } }) => {
   L.marker([lat, lng])
     .addTo(map)
     .on("click", () => {
       L.popup()
         // Make + 0.02 to display popup above the marker
         .setLatLng([lat + 0.02, lng])
-        .setContent("<p>" + name + "</p>")
+        .setContent(
+          "<div>" +
+            `<h3>${name}</h3>` +
+            `<img class="place-image" src="${imageUrl}" alt="${name}" />` +
+            "</div>"
+        )
         .openOn(map);
     });
 });
